4ae4ffeee925bf4b316392e60f4b48caff0c1db7
rostislavsmirnov <rostislavsmrnoff@gmail.com>
2016-10-15 00:28:25 +0300

    tiles generation

---
+++ Mafia/Assets/Script/BoardManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class BoardManager : MonoBehaviour {
+
+
+	public int columns = 8;
+	public int rows = 10;
+	public GameObject floorTile;
+	public GameObject outerWall;
+	private Transform boardHolder;
+	private List <Vector2> gridPositions = new List<Vector2>();
+
+	void InitList() {
+		gridPositions.Clear();
+
+		for (int x = 1; x < columns - 1; x += 1) {
+			for (int y = 1; y < rows - 1; y += 1) {
+				gridPositions.Add(new Vector2(x, y));
+			}
+		}
+	}
+
+	void BoardSetup() {
+		boardHolder = new GameObject ("Board").transform;
+
+		float x_offset = 0;
+		float y_offset = 0;
+		float x = 0;
+		float y = 0;
+
+		for (int i = 0; i < columns; i += 1) {
+
+			x = x_offset;
+			y = y_offset;
+
+			for (int j = 0; j < rows; j += 1) {
+
+				GameObject toInstance = floorTile;
+
+
+				GameObject instance = 
+					Instantiate(toInstance, new Vector2(x, y), Quaternion.identity) as GameObject;
+
+				x += 0.5f;
+				y -= 0.25f;
+
+				instance.transform.SetParent(boardHolder);
+
+			}
+
+			x_offset -= 0.5f;
+			y_offset -= 0.25f; 
+		}
+	}
+
+	Vector2 randomPosition() {
+
+		int randomIndex = UnityEngine.Random.Range(0, gridPositions.Count);
+
+		Vector2 randomPos = gridPositions[randomIndex];
+
+		gridPositions.RemoveAt(randomIndex);
+
+		return randomPos;
+	}
+
+	public void SetupScene() {
+		BoardSetup();
+		InitList();
+	}
+
+	// Use this for initialization
+	void Start () {
+		SetupScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Mafia/Assets/Script/MovingObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+
+	public float moveTime = 0.1f;
+	public LayerMask blockingLayer;
+
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2d;
+	private float inverseMoveTime;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		boxCollider = GetComponent<BoxCollider2D>();
+
+		rb2d = GetComponent<Rigidbody2D>();
+
+		inverseMoveTime = 1f / moveTime;
+	}
+	
+//	protected bool Move (int xDir, int yDir, out RaycastHit2D ray) {
+//		Vector2 start = transform.position;
+//
+//		Vector2 end = start + new Vector2 (xDir, yDir);
+//
+//		boxCollider.enabled = false;
+//
+//
+//	}
+}
